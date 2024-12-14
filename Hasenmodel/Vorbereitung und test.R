bd88088e3e760235c3e398b582f95fb6f2cfd326
Kirsten Flockenhaus <uikxq@student.kit.edu>
2024-12-14 19:06:09 +0100

    neue Kommentierung

--- Hasenmodel/Vorbereitung und test.R
+++ Hasenmodel/Vorbereitung und test.R
@@ -1,31 +1,32 @@
 # Course: Process-based modeling in ecology - WS 24/25
-# Session: Implement the model
+# Model: Reduzierung einer Hasen-Population von Kanninchen durch Inzucht
 # Author: Yarin Gora & Kirsten Flockenhaus
 # Date: 2024-12-14
 
-# Description ----
+# Beschreibung ----
 
-# This R code defines a function for simulating population dynamics using
-# two
-# versions of the Ricker function, one deterministic and the other stochastic.
-# Inzest nur auf growth rate; weniger kanninchen pro Kanninchen 
+# Der R Code definiert eine Simulation für die Dynamik einer Hasen-Population mit möglichem Inzucht. 
+# Dabei wird aufbauend auf die Ricker Funktion eine Funktion für für den Inzucht-Faktor implementiert. 
+# Die Auswirkung der Inzucht wird, als Reduktion der Reproduktionsrate betrachtet. 
+
+# Wie klein muss ein urbanes Areal sein, damit die Inzucht eine Population von Kaninchen
+# reduziert?
 
 # Parameter ----
 
-# N: current population size
-# R: growth rate
+# N: Populationsgroeße
+# R: Reproduktionsrate
 # K: carrying capacity
-# I: incest factor 
-# t: Timestep
-# m: mortalität
-# Functions ----
+# I: Inzucht Faktor 
+# t: Zeitschritte
+# m: Mortalitaet
 
-# Ricker function
-# The first function, "Ricker", calculates the population size at the next time
-# step using a deterministic equation:
+# Ricker Funktion ----
+
+# Die Ricker Funktion kalkuliert die Populationsgröße 
 
 ricker <- function(n, r, k,I) {
-  n_new <- n * exp(r * I*(1 - n / k))
+  n_new <- n * exp(r * I*(1 - n / k)) # Einbau des Inzucht-Faktors
   return(n_new)
 }
 rickerhase <- function(n, r, m, I) {
@@ -35,18 +36,17 @@ rickerhase <- function(n, r, m, I) {
 ricker(18.45997, 1.5, 100,7)
 
 
-# Simulation function for local population dynamics ----
+# Simulation Funktion fuer die Hasen-Population ----
 simulation <- function(timesteps, n_initial, r, k, visualisation) {
   n <- vector(mode = "numeric", length = timesteps) # Vektor starten
   I <- vector(mode = "numeric", length = timesteps)
   n[1] <- n_initial
-  I[1] <- 1 # Startwert für die Carrying Capacity
-  # Schleife, überprüft ob ein Wert im Vektor unter 10, dann die Ricker-Funktion anwenden 
-  # und den k-Wert erhöhen
+  I[1] <- 1 # Startwert für den Inzucht- Faktor
+  # Schleife, mit der Ricker-Funktion über Zeitschritte und Berechnung des Inzest-Faktors
 # while(any(n > 0) ==TRUE){
     for (t in 1:timesteps) {
       n[t + 1] <- ricker(n[t], r, k, I[t])
-      I[t + 1] <- 1-(1/(2*n[t]))*exp(t)
+      I[t + 1] <- 1-(1/(2*n[t]))*exp(t) # Berechnung Inzest-Faktor mit Populationsgröße und Zeitschritte
       if(I[t +1]< 0)
       {I[t + 1] = I[t]}
       #if(n[t+1]<0)
@@ -56,15 +56,15 @@ simulation <- function(timesteps, n_initial, r, k, visualisation) {
     
 #  }
   
-  print(n) # Ausgabe des Vektors n
-  print(I) # Ausgabe vom K-Wert
-  # Visualization of output
+  print(n) # Ausgabe des Vektors n für die Populationsgröße
+  print(I) # Ausgabe vom I-Faktors
+  # Visualisierung 
   if (visualisation == TRUE) {
     plot(n[1:t], 
          type = "l", xlab = "Zeitschritt", ylab = "HASEN", 
          main = "Hasen mit Inzucht 🐇", col.main = "black")
     
-    # Geist-Emoji as points for the plots
+    # Hasen-Emoji als Punkt für die plots
     hasen <- "\U1F407" # 🐇
     text(n[1:t], labels = hasen, cex = 1, col = "brown")
   }
